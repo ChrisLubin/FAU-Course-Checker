89b2452fabadbf46f64ed23b7079038cfcff032d
ChrisLubin <chrisx700@gmail.com>
2019-12-21 23:33:45 -0500

    Surrounded the search fetch request with try/catch

--- functions/search.js
+++ functions/search.js
@@ -2,17 +2,21 @@ const fetch = require('node-fetch');
 const ROOT_URL = require('../models/rootUrl');
 
 async function search(course, cookie) {
-  const res = await fetch(`${ROOT_URL}/searchResults/searchResults?txt_subject=${course.subject}&txt_courseNumber=${course.number}&txt_term=202001`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      cookie
-    },
-    credentials: 'include'
-  });
+  try {
+    const res = await fetch(`${ROOT_URL}/searchResults/searchResults?txt_subject=${course.subject}&txt_courseNumber=${course.number}&txt_term=202001`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        cookie
+      },
+      credentials: 'include'
+    });
 
-  const query = await res.json();
-  return query.data;
+    const query = await res.json();
+    return query.data;
+  } catch (err) {
+    return null;
+  }
 }
 
 module.exports = search;
