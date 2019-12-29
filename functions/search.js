3697304114779f1c4d16718e822b0dc42d70f843
ChrisLubin <chrisx700@gmail.com>
2019-12-29 10:16:00 -0500

    Max amount of course data returned from API is now 50 instead of 20

--- functions/search.js
+++ functions/search.js
@@ -4,7 +4,7 @@ const ROOT_URL = require('../models/rootUrl');
 
 async function search(targetCourse, cookie, semesterId) {
   try {
-    const params = `txt_subject=${targetCourse.subject}&txt_courseNumber=${targetCourse.number}&txt_term=${semesterId}`;
+    const params = `txt_subject=${targetCourse.subject}&txt_courseNumber=${targetCourse.number}&txt_term=${semesterId}&pageMaxSize=50`;
     const res = await fetch(`${ROOT_URL}/searchResults/searchResults?${params}`, {
       method: 'GET',
       headers: {
